2ea1e1d37913b521ea59999396bf1b9eeed5c883
Shinichirow KAMITO <updoor@gmail.com>
2013-09-28 16:06:48 +0900

    JSとCSSをHTMLに埋め込むように修正。

--- lib/k4slide/markdown_compiler.rb
+++ lib/k4slide/markdown_compiler.rb
@@ -10,6 +10,8 @@ module K4slide
     def to_slide(md_src)
       @body = @compiler.markdown.compile(md_src)
       @title = 'example'
+      @js_assets = read_js_assets()
+      @css_assets = read_css_assets()
 
       layout_src = read_layout_template()
       erb = ERB.new(layout_src)
@@ -22,6 +24,24 @@ module K4slide
       layout_template = File.join(layout_dir, 'layout.html.erb')
       src = File.read(layout_template)
       return src
+    end
+
+    def read_js_assets()
+      js_files = ['k4slide.js']
+      return js_files.map do |js_file|
+        File.read(File.join(asset_src_root, js_file))
+      end
+    end
+
+    def read_css_assets()
+      css_files = ['k4slide.css']
+      return css_files.map do |css_file|
+        File.read(File.join(asset_src_root, css_file))
+      end
+    end
+
+    def asset_src_root
+      return File.expand_path(File.join(File.dirname(__FILE__), "../../assets"))
     end
   end
 end
